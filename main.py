89f01ea55a4e9c1d201afd408cc67f72b4cf9301
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-31 19:36:39 +0500

    download photos from NASA
    EPIC

--- main.py
+++ main.py
@@ -84,11 +84,61 @@ def nasa():
             file.write(response.content)
 
 
+def epic():
+    directory = "epic"
+    os.makedirs(directory, exist_ok=True)
+
+    load_dotenv()
+    token = os.environ["NASA_API"]
+
+    url = "https://api.nasa.gov/EPIC/api/natural"
+    params = {
+        "api_key": token,
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as error:
+        print(f"Ошибка при получении данных: {error}")
+        return
+    except ValueError:
+        print("Ошибка при декодировании JSON.")
+        return
+
+    data_list = response.json()
+    base_url = "https://api.nasa.gov/EPIC/archive/natural"
+    image_urls = []
+
+    for data in data_list:
+        date_str = data["date"]  # "2025-01-30 00:03:42"
+        image_name = data["image"]  # "epic_1b_20250130000830"
+
+        date_parts = date_str.split()[0].split("-")
+        year, month, day = date_parts
+
+        image_urls.append(
+            f"{base_url}/{year}/{month}/{day}/png/{image_name}.png?api_key={token}"
+        )
+
+    for i, image_url in enumerate(image_urls, start=1):
+        file_name = f"epic_{i}.png"
+        file_path = os.path.join(directory, file_name)
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            print(f"Ошибка при загрузке изображения {image_url}: {error}")
+            return
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+
+
 def main():
     # fetch_spacex_last_launch()
     url = "https://apod.nasa.gov/apod/image/2501/HubblesVariablecopy1024.jpg"
     # picture_extension(url)
-    nasa()
+    epic()
 
 
 if __name__ == "__main__":

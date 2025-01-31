1b56fa51146c1a9e85826d3226563cf2937b5b65
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-31 15:04:31 +0500

    Написал функцию для скачивания картинок

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import requests
+
+
 def main():
-    pass
+    response = requests.get(
+        "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    )
+    response.raise_for_status()
+    with open("hubble.jpeg", "wb") as file:
+        file.write(response.content)
 
 
 if __name__ == "__main__":

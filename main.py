e3111a0196636fdd9e67842591e388d807941e99
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-31 15:41:18 +0500

    Оформил скачивание картинок как функцию
    И добавил обработку исключений

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import requests
 import os
 
 
-def main():
-    file_path = os.path.join("images", "hubble.jpeg")
-    os.makedirs("images", exist_ok=True)
-
-    response = requests.get(
-        "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    )
-    response.raise_for_status()
+def download_picture(url, file_path):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as error:
+        print(f"Ошибка при загрузке изображения: {error}")
+        return
 
     with open(file_path, "wb") as file:
         file.write(response.content)
+
+
+def main():
+    directory = "images"
+    file_name = "hubble.jpeg"
+    file_path = os.path.join(directory, file_name)
+
+    os.makedirs(directory, exist_ok=True)
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    download_picture(url, file_path)
 
 
 if __name__ == "__main__":

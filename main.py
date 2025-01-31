f1f228cdc796ea0b334b652d6d2547a35e3680a9
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-31 15:21:10 +0500

    Добавил создание папки для картинок

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
+import os
 
 
 def main():
+    file_path = os.path.join("images", "hubble.jpeg")
+    os.makedirs("images", exist_ok=True)
+
     response = requests.get(
         "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
     )
     response.raise_for_status()
-    with open("hubble.jpeg", "wb") as file:
+
+    with open(file_path, "wb") as file:
         file.write(response.content)
 
 

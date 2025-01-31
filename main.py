16b1d9f02290b4ae8d8291dddab416b09c8a0442
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-31 18:31:30 +0500

    Оформил функцию spacex

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import requests
 import os
+from urllib.parse import urlparse
+from dotenv import load_dotenv
 
 
-def download_picture(url, file_path):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as error:
-        print(f"Ошибка при загрузке изображения {url}: {error}")
-        return
-
-    with open(file_path, "wb") as file:
-        file.write(response.content)
-
-
-def main():
+def fetch_spacex_last_launch():
     directory = "images"
     os.makedirs(directory, exist_ok=True)
     url = "https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a"
@@ -38,7 +28,54 @@ def main():
     for i, image_url in enumerate(image_urls, start=1):
         file_name = f"spacex_{i}.jpg"
         file_path = os.path.join(directory, file_name)
-        download_picture(image_url, file_path)
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            print(f"Ошибка при загрузке изображения {image_url}: {error}")
+            return
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+
+
+def picture_extension(url):
+    path = urlparse(url).path
+    filename = os.path.basename(path)
+    name, extension = os.path.splitext(filename)
+    return print(name, extension)
+
+
+def nasa():
+    directory = "nasa"
+    os.makedirs(directory, exist_ok=True)
+
+    load_dotenv()
+    token = os.environ["NASA_API"]
+
+    url = "https://api.nasa.gov/planetary/apod"
+    params = {
+        "api_key": token,
+        "count": 50,
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as error:
+        print(f"Ошибка при получении данных: {error}")
+        return
+    except ValueError:
+        print("Ошибка при декодировании JSON.")
+        return
+
+    return print(response.json())
+
+
+def main():
+    # fetch_spacex_last_launch()
+    url = "https://apod.nasa.gov/apod/image/2501/HubblesVariablecopy1024.jpg"
+    # picture_extension(url)
+    nasa()
 
 
 if __name__ == "__main__":

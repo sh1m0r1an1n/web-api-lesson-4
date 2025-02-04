6aee4453cec2b59a462b8f35d35e9aa2c90cabe3
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-02-05 00:49:53 +0500

    Исправил название переменной

--- download_spacex_launch_images.py
+++ download_spacex_launch_images.py
@@ -5,10 +5,10 @@ from send_get_request import send_get_request
 from download_images import download_images
 
 
-def download_spacex_launch_images(id_spacex_launch):
+def download_spacex_launch_images(spacex_launch_id):
     directory = "SpaceX"
     os.makedirs(directory, exist_ok=True)
-    url = f"https://api.spacexdata.com/v5/launches/{id_spacex_launch}"
+    url = f"https://api.spacexdata.com/v5/launches/{spacex_launch_id}"
 
     response = send_get_request(url)
 
@@ -22,6 +22,6 @@ if __name__ == "__main__":
         description="Программа скачивает фото запуска SpaceX по его id, по умолчанию крайний запуск."
     )
     parser.add_argument("--id", type=str, help="id запуска SpaceX", default="latest")
-    id_spacex_launch = parser.parse_args().id
+    spacex_launch_id = parser.parse_args().id
 
-    download_spacex_launch_images(id_spacex_launch)
+    download_spacex_launch_images(spacex_launch_id)

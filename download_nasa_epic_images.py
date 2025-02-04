9b5b762d87011af4812efa3e2cb0ef6e5a33dc51
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-02-04 23:57:10 +0500

    Добавил защиту от ошибок в GET параметрах

--- download_nasa_epic_images.py
+++ download_nasa_epic_images.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+from urllib.parse import urlparse, urlunparse, urlencode
 
 from send_get_request import send_get_request
 from download_images import download_images
@@ -28,8 +29,14 @@ def download_nasa_epic_images(nasa_api):
         dt = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
         year, month, day = dt.strftime("%Y"), dt.strftime("%m"), dt.strftime("%d")
 
+        url = f"{base_url}/{year}/{month}/{day}/png/{image_name}.png"
+        params = {"api_key": nasa_api}
+
+        parsed_url = urlparse(url)
+        query_string = urlencode(params)
+
         image_urls.append(
-            f"{base_url}/{year}/{month}/{day}/png/{image_name}.png?api_key={nasa_api}"
+            urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params, query_string, parsed_url.fragment))
         )
 
     download_images(image_urls, directory)

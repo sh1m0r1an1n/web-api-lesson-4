eba3a547203482ab9afe0050cc944e6947237a0a
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-02-05 00:34:39 +0500

    Разбил функции на части

--- download_nasa_epic_images.py
+++ download_nasa_epic_images.py
@@ -7,18 +7,7 @@ from send_get_request import send_get_request
 from download_images import download_images
 
 
-def download_nasa_epic_images(nasa_api):
-    directory = "NASA_EPIC"
-    os.makedirs(directory, exist_ok=True)
-
-    url = "https://api.nasa.gov/EPIC/api/natural"
-    params = {
-        "api_key": nasa_api,
-    }
-
-    response = send_get_request(url, params)
-
-    data_list = response.json()
+def generate_image_urls_from_response_nasa_epic(data_list, params):
     base_url = "https://api.nasa.gov/EPIC/archive/natural"
     image_urls = []
 
@@ -30,7 +19,6 @@ def download_nasa_epic_images(nasa_api):
         year, month, day = dt.strftime("%Y"), dt.strftime("%m"), dt.strftime("%d")
 
         url = f"{base_url}/{year}/{month}/{day}/png/{image_name}.png"
-        params = {"api_key": nasa_api}
 
         parsed_url = urlparse(url)
         query_string = urlencode(params)
@@ -38,6 +26,20 @@ def download_nasa_epic_images(nasa_api):
         image_urls.append(
             urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params, query_string, parsed_url.fragment))
         )
+    return image_urls
+
+
+def download_nasa_epic_images(nasa_api):
+    directory = "NASA_EPIC"
+    os.makedirs(directory, exist_ok=True)
+
+    url = "https://api.nasa.gov/EPIC/api/natural"
+    params = {"api_key": nasa_api}
+
+    response = send_get_request(url, params)
+    data_list = response.json()
+
+    image_urls = generate_image_urls_from_response_nasa_epic(data_list, params)
 
     download_images(image_urls, directory)
 
--- launch_telegram_bot.py
+++ launch_telegram_bot.py
@@ -6,6 +6,33 @@ from environs import Env
 import telegram
 
 
+def generate_image_paths():
+    directories = ["NASA_APOD", "NASA_EPIC", "SpaceX"]
+    image_paths = []
+
+    for directory in directories:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.lower().endswith(
+                    (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp")
+                ):
+                    image_paths.append(os.path.join(root, file))
+    return image_paths
+
+
+def send_images_without_ending(bot, channel_id, image_paths, seconds):
+    MAX_FILE_SIZE_MB = 20
+    MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024
+
+    while True:
+        for image_path in image_paths:
+            if os.path.getsize(image_path) < MAX_FILE_SIZE_BYTES:
+                with open(image_path, "rb") as photo:
+                    bot.send_photo(chat_id=channel_id, photo=photo)
+                    time.sleep(seconds)
+        random.shuffle(image_paths)
+
+
 def launch_telegram_bot(env):
     parser = argparse.ArgumentParser(
         description="Программа запускает бота для публикации фото из директорий проекта в телеграм канал."
@@ -22,29 +49,10 @@ def launch_telegram_bot(env):
     bot = telegram.Bot(bot_token)
     channel_id = env.str("TG_CHANNEL_ID")
 
-    directories = ["NASA_APOD", "NASA_EPIC", "SpaceX"]
-    image_paths = []
-
-    for directory in directories:
-        for root, _, files in os.walk(directory):
-            for file in files:
-                if file.lower().endswith(
-                    (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp")
-                ):
-                    image_paths.append(os.path.join(root, file))
-
+    image_paths = generate_image_paths()
     random.shuffle(image_paths)
 
-    MAX_FILE_SIZE_MB = 20
-    MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024
-
-    while True:
-        for image_path in image_paths:
-            if os.path.getsize(image_path) < MAX_FILE_SIZE_BYTES:
-                with open(image_path, "rb") as photo:
-                    bot.send_photo(chat_id=channel_id, photo=photo)
-                    time.sleep(seconds)
-        random.shuffle(image_paths)
+    send_images_without_ending(bot, channel_id, image_paths, seconds)
 
 
 if __name__ == "__main__":
